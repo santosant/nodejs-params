6215d686d00b8138bcd8dbdb3978bc21b4b0b031
Antonio Anderson <hiroto.anderson@gmail.com>
2021-02-01 12:16:02 -0300

    middleware functions

--- src/index.js
+++ src/index.js
@@ -1,11 +1,48 @@
 const express = require('express');
 const { v4: uuidv4 } = require('uuid'); //id unico universal
+const { v4: isUuid } = require('uuid');
 
 const app = express();
 
 app.use(express.json());
 
 const projects = [];
+
+function logRequests(request, response, next) {
+    const { method, url } = request;
+
+    /**
+     * O método "toUppercase" precisa ser chamado sem os parênteses para que não 
+     * apresente erro no insomnia, por exemplo
+     * 
+     * Antes:
+     * const logLabel = `[${method.toUppercase()} ${url}]`;
+     * 
+     * Depois:
+     * const logLabel = `[${method.toUppercase} ${url}]`;
+     */
+
+    const logLabel = `[${method.toUppercase} ${url}]`;
+
+    console.time(logLabel);
+
+    next(); // Próximo middleware
+
+    console.timeEnd(logLabel);
+}
+
+function validateProjectId(request, response, next) {
+    const { id } = request.params;
+
+    if (!isUuid(id)) {
+        return response.status(400).json({ error: 'Invalid project ID.' });
+    }
+
+    return next();
+}
+
+app.use(logRequests);
+app.use('/projects/:id', validateProjectId);
 
 app.get('/projects', (request, response) => {
     const { title } = request.query;
